e2aecb4290ec779808f4e6e7de4bf5d2616f12c0
Shallon Kobusinge <kobshallon041@gmail.com>
2022-03-07 19:07:34 +0200

    feat: post crud apis

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -13,8 +13,8 @@ export const create = async (req: Request, res: Response) => {
     const { type, category , datePostedOn, title, content, dateUpdatedOn, status, author } = req.body;
     
 
-    const userTypeRepository = await getRepository(UserTypes);
-    const userType = await userTypeRepository.findOne({ id: Number(type) });
+    const userRepository = await getRepository(UserTypes);
+    const userType = await userRepository.findOne({ id: Number(type) });
     if(!userType) {
         return res.status(400).send(ERROR_RESPONSE("User type not found"));
     }
@@ -36,7 +36,7 @@ export const create = async (req: Request, res: Response) => {
     const postRepository = await getRepository(Posts);
     const existingPost = await postRepository.findOne({title});
     if(existingPost){
-         return res.status(400).send(ERROR_RESPONSE(`Post with tiles ${existingPost.title} already exists`));
+         return res.status(400).send(ERROR_RESPONSE(`Post with tiles ${title} already exists`));
     }
 
     const post = await postRepository.create({
@@ -53,4 +53,65 @@ export const create = async (req: Request, res: Response) => {
     const savedPost = await postRepository.save(post);
     return res.status(201).send(SUCCESS_RESPONSE("Post created successfully", savedPost));
 
+}
+
+
+export async function single(req: Request, res: Response){
+  const post = await getOne(req.params.id);
+  if(post === false){
+      return res.status(400).send(ERROR_RESPONSE("Post not found"))
+  }
+  return res.status(200).send(SUCCESS_RESPONSE("", post));
+}
+
+
+export async function all(req: Request, res: Response){
+   const postRepository = getRepository(Posts);
+   const posts = await postRepository.find();
+   return res.status(200).send(SUCCESS_RESPONSE("", posts))
+}
+
+
+export async function update(req: Request, res: Response){
+    const { type, category , datePostedOn, title, content, dateUpdatedOn, status, author } = req.body;
+     const postRepository = getRepository(Posts);
+     const post = await getOne(req.params.id);
+      if(post === false){
+      return res.status(400).send(ERROR_RESPONSE("Post not found"))
+  }
+  await postRepository.update(Number(req.params.id),{
+        type,
+        category,
+        datePostedOn: new Date(datePostedOn),
+        dateUpdatedOn: new Date(dateUpdatedOn),
+        title,
+        content,
+        status,
+        author
+    })
+  const updatedPost = await postRepository.find({ id: Number(req.params.id)})
+  return res.status(200).send(SUCCESS_RESPONSE("Post updated successfully", updatedPost));
+}
+
+export async function deletePost(req: Request, res: Response){
+     const postRepository = getRepository(Posts);
+     const post = await getOne(req.params.id);
+      if(post === false){
+      return res.status(400).send(ERROR_RESPONSE("Post not found"));
+    }
+    await postRepository.remove(post);
+    return res.send(`Post with id ${req.params.id} deleted successfully`);
+
+}
+
+export const getOne = async(id)=>{
+const postRepository = getRepository(Posts);
+   const post = await postRepository.find({
+       id: Number(id)
+   })
+    if(!post){
+        return false;
+    }
+    return post;
+    
 }
